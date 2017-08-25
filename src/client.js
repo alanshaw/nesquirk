69dcd13c36c615b4219d70b7529e6aba353dc275
Alan Shaw <alan@tableflip.io>
2017-08-25 10:11:22 +0100

    Reorganise so client features are also exposed on the server

---
+++ src/client.js
@@ -0,0 +1,6 @@
+export { default as Client } from './client/Client'
+export { default as Collection } from './client/Collection'
+
+export { default as Provider } from './client/Provider'
+export { default as withClient } from './client/withClient'
+export { default as createContainer } from './client/createContainer'
